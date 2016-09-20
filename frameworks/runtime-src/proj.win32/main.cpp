6bbc7091d95867fde83f388f5e723afc0a5ffe4f
GAMEABC\zhoupengfei01 <zhoupengfei01@bianfeng.com>
2016-09-20 15:50:32 +0800

    提交图片资源

---
+++ frameworks/runtime-src/proj.win32/main.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "SimulatorWin.h"
+#include <shellapi.h>
+
+// uncomment below line, open debug console
+#define USE_WIN32_CONSOLE
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+#ifdef USE_WIN32_CONSOLE
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+#endif
+
+    int ret = SimulatorWin::getInstance()->run();
+
+    return ret;
+}
